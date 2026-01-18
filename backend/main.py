fdad5b0b6fe692247e371a3d32a77c69f764404c
juliasvgomes <juliasvgomes@gmail.com>
2026-01-18 17:57:32 -0300

    Update API URL for production

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",          # frontend local
-        "https://email-classifier-sage-gamma.vercel.app/"    # frontend em produção
+        "https://email-classifier-sage-gamma.vercel.app"    # frontend em produção
         ],
     allow_credentials=True,
     allow_methods=["*"],
